b7235d05f871ab7d8c9fd01bebec8ea68f31914a
Lars Simkins <lars.simkins@gmail.com>
2014-11-16 17:07:58 -0800

    Stacks

---
+++ Assets/Scripts/GameWorld/WIBaseScripts/WIStack.cs
@@ -0,0 +1,332 @@
+using UnityEngine;
+using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using Frontiers.World;
+
+namespace Frontiers {
+
+	[Serializable]
+	public class WIStack
+	{
+		public WIStack ( )
+		{
+			LockRefreshAction = false;
+		}
+
+		public virtual WIStackMode Mode {
+			get {
+				return mMode;
+			}
+			set {
+				//TODO update state based on mode
+				mMode = value;
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		public virtual WIGroup Group {
+			get {
+				return mGroup;
+			}
+			set {
+				//TODO set group on items (?)
+				mGroup = value;
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		public System.Action RefreshAction {
+			get {
+				return mRefreshAction;
+			}
+			set {
+				//if (!LockRefreshAction) {
+				mRefreshAction = value;
+				//} else {
+					//Debug.Log ("Tried to set refresh action in stack and can't");
+				//}
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		public System.Action DestroyedAction {
+			get {
+				return mDestroyedAction;
+			}
+			set {
+				mDestroyedAction = value;
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		[NonSerialized]
+		public WIStackContainer Container;
+
+		public bool SendCurrencyToBank {
+			get {
+				return Bank != null;
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		[NonSerialized]
+		public IBank Bank;
+
+		[XmlIgnoreAttribute]
+		public virtual IStackOwner Owner {
+			get {
+				if (BelongsToContainer) {
+					return Container.Owner;
+				}
+				return null;
+			}
+			set {
+				return;
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		public virtual List <IWIBase> Items {
+			get {
+				if (mItems == null) {
+					mItems = new List <IWIBase> ();
+				}
+				return mItems;
+			}
+		}
+
+		public string GroupPath {
+			get {
+				if (mGroup == null) {
+					return WIGroups.Get.World.Path;
+				}
+				return mGroup.Path;
+			}
+			set {
+				WIGroups.FindGroup (value, out mGroup);
+				for (int i = 0; i < Items.Count; i++) {
+					if (Items [i] != null) {
+						Items [i].Group = mGroup;
+					}
+				}
+			}
+		}
+
+		public virtual void Clear () 
+		{
+			if (mItems != null) {
+				mItems.Clear ();
+			}
+			mItems = null;
+		}
+
+		public void SetItems (List <IWIBase> items, bool clearExisting)
+		{
+			if (mItems == null) {
+				mItems = items;
+				return;
+			} else if (mItems.Count == 0) {
+				mItems = null;
+				mItems = items;
+				return;
+			} else if (clearExisting) {
+				mItems.Clear ();
+				mItems = null;
+			}
+			mItems = items;
+
+			if (Group == WIGroups.Get.Player) {
+				for (int i = 0; i < mItems.Count; i++) {
+					if (mItems [i].IsQuestItem) {
+						Player.Local.Inventory.AddQuestItem (mItems [i].QuestName);
+					}
+				}
+			}
+		}
+
+		//do not touch these xml attributes
+		//saving and loading will explode
+		[XmlArray ("SerializableItems", IsNullable = true)]
+		[XmlArrayItem ("StackItem", Type = typeof(StackItem), IsNullable = true)]
+		public virtual StackItem [] SerializableItems {
+			get {
+				if (mItems == null || mItems.Count == 0) {
+					return null;
+				}
+				StackItem [] stackItemArray = new StackItem [Items.Count];
+				for (int i = 0; i < Items.Count; i++) {
+					stackItemArray [i] = mItems [i].GetStackItem (WIMode.None);
+				}
+				return stackItemArray;
+			}
+			set {
+				if (value != null) {
+					for (int i = 0; i < value.Length; i++) {
+						StackItem stackItem = value [i];
+						stackItem.Group = mGroup;
+						Items.Add (value [i]);
+					}
+					Array.Clear (value, 0, value.Length);
+				}
+			}
+		}
+
+		public bool Disabled {
+			get {
+				return mDisabled;
+			}
+			set {
+				if (mDisabled != value) {
+					mDisabled = value;
+					Refresh ();
+				}
+			}
+		}
+
+		public WISize StackMaxSize = WISize.Huge;
+
+		[XmlIgnoreAttribute]
+		[NonSerialized]
+		public bool LockRefreshAction = false;
+
+		public virtual bool BelongsToContainer {
+			get {
+				return Container != null;
+			}
+		}
+
+		public virtual bool HasOwner (out IStackOwner owner)
+		{
+			owner = null;
+			if (BelongsToContainer) {
+				return Container.HasOwner (out owner);
+			}
+			return false;
+		}
+
+		public virtual WISize Size {
+			get {
+				if (Mode == WIStackMode.Enabler) {
+					return WISize.NoLimit;
+				}
+				IStackOwner owner = null;
+				if (HasOwner (out owner)) {
+					return owner.Size;
+				}
+				return StackMaxSize;
+			}
+		}
+
+		public virtual IWIBase TopItem {
+			get {
+				return Items [Items.Count - 1];
+			}
+		}
+
+		public virtual bool HasTopItem {
+			get {
+				return NumItems > 0;
+			}
+		}
+
+		public virtual int NumItems {
+			get {
+				if (Disabled) {
+					return 0;
+				}
+				return Items.Count;
+			}
+		}
+
+		public virtual int SpaceLeft {
+			get {
+				return MaxItems - NumItems;
+			}
+		}
+
+		public virtual int MaxItems {
+			get {
+				int maxItems = 1;
+
+				if (Disabled) {
+					maxItems = 0;
+				} else {
+					switch (Mode) {
+					case WIStackMode.Enabler:
+					case WIStackMode.Wearable:
+					case WIStackMode.Recepticle:
+	//				//Debug.Log ("is wearable / enabler - max items is 1");
+						break;
+
+					case WIStackMode.Generic:
+						if (HasTopItem) {
+							maxItems = Stackable.MaxItemsFromSize (TopItem.Size);
+	//					//Debug.Log ("Has top item, max items is " + maxItems.ToString ( ));
+						} else {
+							//this will only be true until the top item is empty
+							maxItems = Stackable.MaxItemsFromSize (WISize.Tiny);
+	//					//Debug.Log ("Is empty, max items is " + maxItems.ToString ( ));
+						}
+						break;
+
+					default:
+						break;
+					}
+				}
+				return maxItems;
+			}
+		}
+
+		public virtual bool IsEmpty {
+			get {
+				return NumItems == 0;
+			}
+		}
+
+		public virtual bool IsFull {
+			get {
+				return (NumItems >= MaxItems) || Disabled;
+			}
+		}
+
+		public virtual void OnItemRemoved ()
+		{	//this will automatically result in a Refresh call
+			////Debug.Log ("WISTACK: Called OnItemRemoved... clearing dead weight");
+			Stacks.Clear.DestroyedOrMovedItems (this);
+		}
+
+		public virtual void OnItemAdded ()
+		{	//this will automatically result in a Refresh call
+			Stacks.Clear.DestroyedOrMovedItems (this);
+		}
+
+		public virtual void OnDestroyed ()
+		{	//stacks will take care of clearing the item list
+			//just tell your owner that you've been destroyed
+			mDestroyedAction.SafeInvoke ();
+		}
+
+		public virtual void Refresh ()
+		{
+			////Debug.Log ("WISTACK: Refreshing stack...");
+			mRefreshAction.SafeInvoke ();
+			if (BelongsToContainer) {	////Debug.Log ("WISTACK: calling refresh on container...");
+				Container.Refresh ();
+			}
+		}
+
+		[XmlIgnore]
+		protected WIGroup mGroup;
+		[XmlIgnore]
+		protected System.Action mRefreshAction = null;
+		[XmlIgnore]
+		protected System.Action mDestroyedAction = null;
+		protected WIStackMode mMode = WIStackMode.Generic;
+		protected bool mDisabled = false;
+		protected List <IWIBase> mItems;
+	}
+}
---
+++ Assets/Scripts/GameWorld/WIBaseScripts/WIStackContainer.cs
@@ -0,0 +1,238 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using Frontiers;
+using Frontiers.World;
+
+namespace Frontiers {
+
+	[Serializable]
+	public class WIStackContainer : WIStack
+	{
+		public WIStackContainer ( )
+		{
+			mRefreshContainerAction = Refresh;
+			SetStackList (mStackList);
+		}
+		
+		public WIStackContainer (IStackOwner owner)
+		{
+			mOwner = owner;
+			mRefreshContainerAction = Refresh;
+		}
+		
+		public WIStackContainer GetDuplicate (IStackOwner owner)
+		{
+			return new WIStackContainer (owner);
+		}	
+
+		public override WIStackMode Mode {
+			get {
+				return mMode;
+			}
+			set {
+				mMode = value;
+				for (int i = 0; i < StackList.Count; i++) {
+					StackList [i].Mode = mMode;
+				}
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		public override WIGroup Group {
+			get {
+				return mGroup;
+			}
+			set {
+				if (mGroup != value) {
+					mGroup = value;
+					for (int i = 0; i < StackList.Count; i++) {
+						StackList [i].Group = value;
+					}
+				}
+			}
+		}
+
+		[XmlIgnoreAttribute]
+		public override IStackOwner Owner
+		{
+			get {
+				return mOwner;
+			}
+			set {
+				//TODO set owner on items (?)
+				mOwner = value;
+			}
+		}
+
+		public string GroupPath {
+			get {
+				if (mGroup != null) {
+					return mGroup.Path;
+				}
+				return null;
+			}
+			set {
+				if (value != null) {
+					WIGroup group = null;
+					//TODO verify that this works
+					if (WIGroups.FindGroup (value, out group)) {
+						Group = group;
+					}
+				}
+			}
+		}
+
+		public string OwnerPath {
+			get {
+				if (mOwner != null && mOwner.IsWorldItem) {
+					return mOwner.worlditem.StaticReference.FullPath;
+				}
+				return null;
+			}
+			set {
+				if (value != null) {
+					//Debug.Log ("WISTACKCONTAINER: SETTING OWNER IN WISTACKCONTAINER");
+				}
+			}
+		}
+
+		public List <WIStack> StackList {
+			get {
+				return mStackList;
+			}
+			set {
+				SetStackList (value);
+			}
+		}
+
+		public string StackName
+		{
+			get {
+				return mOwner.StackName;
+			}
+		}
+
+		public override WISize Size
+		{
+			get {
+				return Owner.Size;
+			}
+		}
+
+		public override bool HasTopItem {
+			get {
+				if (StackList.Count > 0) {
+					return StackList [0].HasTopItem;
+				}
+				return false;
+			}
+		}
+
+		public bool HasEmptyStackList
+		{
+			get {
+				for (int i = 0; i < StackList.Count; i++) {
+					if (StackList [i].IsEmpty) {
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+
+		public override IWIBase TopItem
+		{
+			get {
+				IWIBase firstItem = null;
+				Stacks.Find.FirstItem (this, out firstItem);
+				return firstItem;
+			}
+		}
+
+		public int NumStacks
+		{
+			get {
+				if (StackList != null) {
+					if (mNumStacks >= 0) {
+						return mNumStacks;
+					}
+					return StackList.Count;
+				}
+				return 0;
+			}
+		}
+
+		public override int NumItems
+		{
+			get {
+				int numItems = 0;
+				foreach (WIStack stack in StackList) {
+					numItems += stack.NumItems;
+				}
+				return numItems;
+			}
+		}
+
+		public override bool HasOwner (out IStackOwner owner)
+		{
+			owner = mOwner;
+			return mOwner != null;
+		}
+
+		public override void Refresh ( )
+		{
+			mRefreshAction.SafeInvoke ();
+		}
+
+		public void SetNumStacks (int numStacks)
+		{	//this sets the mode for any stacks above this index to Disabled
+			numStacks = Mathf.Clamp (numStacks, 1, Globals.MaxStacksPerContainer);
+			for (int i = 0; i < StackList.Count; i++) {
+				if (i < numStacks) {
+					StackList [i].Disabled = false;
+				} else {
+					StackList [i].Disabled = true;
+				}
+			}
+			mNumStacks = numStacks;
+		}
+
+		public void SetStackList (List <WIStack> stackList)
+		{
+			if (mStackList != null && stackList != mStackList) {
+				mStackList = null;
+			}
+			mStackList = stackList;
+			for (int i = 0; i < StackList.Count; i++) {
+				WIStack stack = StackList [i];
+				//tell the stacks to report to this object
+				//other objects will have to subscribe to the container
+				stack.RefreshAction += mRefreshContainerAction;
+				stack.Container	= this;
+				stack.Mode = mMode;
+			}
+		}
+
+		public override void Clear ()
+		{
+			for (int i = 0; i < StackList.Count; i++) {
+				StackList [i].Clear ();
+			}
+			StackList.Clear ();
+		}
+
+		protected List <WIStack> mStackList = new List <WIStack> ( );
+
+		[NonSerialized]
+		protected System.Action mRefreshContainerAction;
+		[NonSerialized]
+		protected IStackOwner mOwner;
+		[NonSerialized]
+		protected int mNumStacks = -1;
+	}
+}
